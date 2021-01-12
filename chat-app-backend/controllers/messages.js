89327a1fb614fc31c25f5e76d6aae77205d79ee9
Henna <henna.honkanen1@gmail.com>
2021-01-12 03:53:04 +0200

    backend ok

--- chat-app-backend/controllers/messages.js
+++ chat-app-backend/controllers/messages.js
@@ -1,10 +1,55 @@
 const messagesRouter = require('express').Router()
 const Message = require('../models/message')
+const User = require('../models/user')
+const bodyParser = require('body-parser')
+messagesRouter.use(bodyParser.json())
+const jwt = require('jsonwebtoken')
 
-messagesRouter.get('/', (request, response) => {
-    Message.find({}).then(messages => {
-      response.json(messages.map(message => message.toJSON()))
-    })
+messagesRouter.get('/', async (request, response) => {
+  const messages = await Message
+  .find({}).populate('user', { username: 1, name: 1 })
+
+  response.json(messages.map(message => message.toJSON()))
 })
+
+messagesRouter.post('/', async (request, response, next) => {
+  const body = request.body
+
+  let message = null
+  let user = null
+
+  if (!request.token) {
+    message = new Message({
+      content: body.content,
+      user: user,
+      date: Date.now()
+    })
+
+  } else {
+
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    const user = await User.findById(decodedToken.id)
+
+    message = new Message({
+      content: body.content,
+      user: user._id,
+      date: Date.now()
+    })
+  }
+  
+  try {
+    const savedMessage = await message.save()
+      
+    if(user !== null){
+      user.messages = user.messages.concat(savedMessage._id)
+      await user.save()  
+    }
+
+      response.json(savedMessage.toJSON())
+    } catch(exception) {
+      next(exception)
+    }
+})
+
 
 module.exports = messagesRouter
--- chat-app-backend/controllers/users.js
+++ chat-app-backend/controllers/users.js
@@ -1,10 +1,46 @@
+const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
-usersRouter.get('/', (request, response) => {
-    User.find({}).then(users => {
-      response.json(users.map(user => user.toJSON()))
+usersRouter.get('/', async (request, response) => {
+  const users = await User
+  .find({}).populate('messages')
+  response.json(users.map(user => user.toJSON()))
+})
+
+usersRouter.post('/', async (request, response, next) => {
+    const users = await User.find({})
+    const usernames = users.map(user=> user.username)
+    const body = request.body
+
+    console.log('users: ' + users)
+  
+    if(body.password.length < 3){
+      return response.status(400).json({ error: 'password must be at least 3 characters long' })
+    }
+  
+    if(body.username.length < 3){
+      return response.status(400).json({ error: 'username must be at least 3 characters long' })
+    }
+
+    if(usernames.includes(body.username)){
+      return response.status(400).json({ error: 'username is already taken' })
+    }
+  
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(body.password, saltRounds)
+  
+    const user = new User({
+      username: body.username,
+      passwordHash,
     })
+  
+    try{
+      const savedUser = await user.save()
+      response.json(savedUser)
+    } catch(error) {
+      next(error)
+    }
 })
 
 module.exports = usersRouter
